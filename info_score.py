8c9c5947c4d56df0874ae36207d2d70935f2349d
minh-phung <118701499+minh-phung@users.noreply.github.com>
2026-03-31 23:55:15 -0400

    update

--- info_score.py
+++ info_score.py
@@ -1,91 +1,90 @@
 import numpy as np
-import pandas as pd
-import time
+
+import infomeasure as im
 import sklearn.feature_selection as fs
 
-def corr_coef(index_pair, data):
-    cor_vec = np.full(len(index_pair), np.nan)
-    for i, each in enumerate(index_pair):
-        x = data[each[0]]
-        y = data[each[1]]
-        cor_vec[i] = np.corrcoef(x, y)[0, 1]
 
-    return cor_vec
+def corr_coef(x, y,
+              variable_x = None, variable_y = None):
+    return np.corrcoef(x, y)[0, 1]
 
-def discrete_bin(data, bin_choice, parameter_0):
+# -----------------------------------------------------------------------------
 
-    if bin_choice == 'q':
-        label = range(parameter_0)
-        return pd.qcut(data, q = parameter_0, labels = label).to_numpy()
+def mic_c_ksg(x, y, x_discrete, y_discrete, k_value,
+              variable_x = None, variable_y = None):
 
-    return
+    if not x_discrete and not y_discrete:
+    # Kraskov–Stögbauer–Grassberger (KSG)
+        mutual_info = im.estimator(x, y, measure = 'mi',
+                                   approach = 'ksg', k = k_value).result()
+        return mutual_info
+    return np.nan
 
-def mic_discrete(index_pair, data,
-                 variable_list = None, variable_discrete = None,
-                 bin_choice = None, parameter_0 = None):
+def mic_m_ross(x, y, x_discrete, y_discrete, k_value,
+               variable_x = None, variable_y = None):
 
-    for i, each_pair in enumerate(index_pair):
-        print(each_pair)
-        # ------------------------------------
+    if x_discrete and not y_discrete:
+        return fs.mutual_info_classif(y.reshape(-1,1), x,
+                                      discrete_features = y_discrete,
+                                      n_neighbors= k_value)[0]
+    elif not x_discrete and y_discrete:
+        return fs.mutual_info_classif(x.reshape(-1,1), y,
+                                      discrete_features = x_discrete,
+                                      n_neighbors= k_value)[0]
+    return np.nan
 
-        x = data[each_pair[0]].to_numpy()
-        x_index = np.where(variable_list == each_pair[0])[0]
-        x_discrete = variable_discrete[x_index]
-        '''
-        x_adjusted = np.full(len(x), np.nan)
-        if x_discrete:
-            x_adjusted = x
-        else:
-            print("x discretized")
-            x_adjusted = discrete_bin(x, bin_choice, parameter_0)
-        '''
-        # ------------------------------------
-
-        y = data[each_pair[1]].to_numpy()
-        y_index = np.where(variable_list == each_pair[1])[0]
-        y_discrete = variable_discrete[y_index]
-
-        y_adjusted = np.full(len(y), np.nan)
-        if y_discrete:
-            y_adjusted = y
-        else:
-            print("y discretize")
-            print(y)
-            print(bin_choice)
-            print(parameter_0)
-            y_adjusted = discrete_bin(y, bin_choice, parameter_0)
-            print(y_adjusted)
-        # ------------------------------------
-
-
-    return
+# -----------------------------------------------------------------------------
 
 def compute(value, index_pair, data,
-            variable_list = None, variable_discrete = None):
+              variable_list = None, variable_discrete = None):
 
     if index_pair.shape[1] != 2:
-        exit("index pair issue")
-    else:
-        try:
-            # -------------------------------------
-            if value == 'corr':
-                return corr_coef(index_pair, data)
-        except:
-            exit("corr issue")
-            # -------------------------------------
-        try:
-            if value.startswith('mic'):
-                if all(variable_discrete) :
-                    return mic_discrete(index_pair, data,
-                                        variable_list, variable_discrete)
-                else:
-                    mic_type = value.split("_")
-                    print(mic_type)
-                    return mic_discrete(index_pair, data,
-                                        variable_list, variable_discrete,
-                                        bin_choice = mic_type[1],
-                                        parameter_0 = int(mic_type[2]))
-        except:
-            exit("mic issue")
+        raise ValueError("Index pair shape issue")
 
-    return
+    result = np.full(len(index_pair), np.nan)
+
+    # ----------------------------------
+    if value == 'corr':
+        def func(x, y,
+                 variable_x = None, variable_y = None):
+            return corr_coef(x, y)
+
+    elif value.startswith('mic'):
+        # "mic,type,parameter" (symmetric)
+        # type:
+        # c := continuous - continuous
+        # d := continuous - discrete
+
+        def func(x, y,
+                 variable_x = None, variable_y = None):
+            try:
+                _, mic_type, mic_param = value.split(',')
+                mic_param = int(mic_param)
+            except:
+                raise ValueError("Invalid mic format")
+
+            x_idx = np.where(variable_x == variable_list)[0]
+            y_idx = np.where(variable_y == variable_list)[0]
+
+            x_discrete = variable_discrete[x_idx]
+            y_discrete = variable_discrete[y_idx]
+
+            if mic_type == 'c_ksg':
+                return mic_c_ksg(x, y, x_discrete, y_discrete, mic_param)
+            elif mic_type == 'm_ross':
+                return mic_m_ross(x, y, x_discrete, y_discrete, mic_param)
+            return np.nan
+    else:
+        raise ValueError("Unknown info_val parameter")
+
+    # ----------------------------------
+
+    result = np.full(len(index_pair), np.nan)
+
+    for i, (var1, var2) in enumerate(index_pair):
+        x = data[var1].to_numpy()
+        y = data[var2].to_numpy()
+
+        result[i] = func(x, y, var1, var2)
+
+    return result
--- main.py
+++ main.py
@@ -9,7 +9,10 @@ import os
 
 name = "data.csv"
 data_file = "data"
-#preprocess.inner_join(['Code', 'Year'], data_file, name)
+
+'''
+preprocess.inner_join(['Code', 'Year'], data_file, name)
+'''
 
 df = pd.read_csv(name)
 print(df.columns)
@@ -42,10 +45,11 @@ column_discrete = np.concatenate((target_discrete, predictor_discrete))
 
 plot_file = 'data plot'
 
-#preprocess.plot_hist(df[predictor], plot_file)
-#preprocess.plot_hist(df[target], plot_file)
-#preprocess.plot_scatter(df[target], df[predictor], plot_file)
-
+'''
+preprocess.plot_hist(df[predictor], plot_file)
+preprocess.plot_hist(df[target], plot_file)
+preprocess.plot_scatter(df[target], df[predictor], plot_file)
+'''
 # -------------------------------------------------------------------
 # rolling cross validation
 
@@ -53,7 +57,9 @@ sampling_file = 'sampling'
 test_train_fraction = 0.25
 k = 5
 
-#sampling.time_series_cv_split(df['Year'], test_train_fraction, k, sampling_file)
+'''
+sampling.time_series_cv_split(df['Year'], test_train_fraction, k, sampling_file)
+'''
 
 cv_test = []
 cv_train = []
@@ -61,33 +67,57 @@ cv_train = []
 for each in os.listdir(sampling_file):
     for i in range(0, k):
         name_search = "cv_" + str(i)
-        dir_str = os.path.join(sampling_file, each)
-        try:
-            dir_data = pd.read_csv(dir_str).to_numpy().flatten()
-            if each == name_search + "_train.csv":
-                cv_train.append(dir_data)
-            elif each == name_search + "_test.csv":
-                cv_test.append(dir_data)
-        except:
-            exit('csv issue')
-# -------------------------------------------------------------------
 
-info_score_file = 'info score'
+        dir_str = os.path.join(sampling_file, each)
+        dir_data = pd.read_csv(dir_str).to_numpy().flatten()
+
+        if each == name_search + "_train.csv":
+            cv_train.append(dir_data)
+        elif each == name_search + "_test.csv":
+            cv_test.append(dir_data)
+
+# -------------------------------------------------------------------
+info_name = "info.csv"
+
+info_corr = ['corr']
+
+info_mic = ['mic,c_ksg,4' , 'mic,c_ksg,6', 'mic,c_ksg,9', 'mic,c_ksg,13',
+            'mic,c_ksg,17', 'mic,c_ksg,22', 'mic,c_ksg,28', 'mic,c_ksg,35',
+            'mic,m_ross,4']
+
+'''
+info_val = np.concatenate((info_corr, info_mic))
 
 info_index = pd.MultiIndex.from_product([range(0, k), target, predictor])
 
-info_val = ['corr', 'mic_q_10']
 info = pd.DataFrame(index=info_index, columns = info_val)
 
-for i in range(0, 1): # fix k
+
+for i in range(0, k): # fix k
     train_set = df.iloc[cv_train[i]]
 
     for each_val in info_val:
-        each_index = info.loc[i, each_val].index.to_frame(index = False).to_numpy()
+        pair_label = info.loc[i, each_val].index.to_frame(index = False).to_numpy()
 
-        info.loc[i, each_val] = info_score.compute(each_val, each_index, train_set,
+        info.loc[i, each_val] = info_score.compute(each_val, pair_label, train_set,
                                                    column_name, column_discrete)
 
-    print(info.loc[i])
+
+info.to_csv(info_name)
+
+'''
+
+info = pd.read_csv(info_name)
+info = info.set_index(info.columns[:3].tolist())
+
+info_plot = 'info plot'
+
+'''
+for i in range(0, 1):
+    preprocess.plot_scatter(info.loc[i, info_corr], info.loc[i, info_mic],
+                            info_plot, tail = 'cv' + str(i))
+'''
+
+# -------------------------------------------------------------------
 
 
